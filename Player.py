800bcd12f10a84f194a1080fead652f2368cee4c
Andreirl032 <andrei.ramos.lopes@gmail.com>
2025-02-26 21:11:50 -0300

    removidos arquivos nao utilizados

--- Player.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self,attack,defense,life,maxItems):
-        self.attack=attack
-        self.defense=defense
-        self.life=life
-        self.maxItems=maxItems
-        self.items=[]
-        self.armor=None
